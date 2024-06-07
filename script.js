21f1edf699bf6db9363426f2086f02097546dc71
nacht <arosales@crackthecode.la>
2024-06-06 19:52:59 -0500

    Versionm065/06/24

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log(10);
+
+/*function suma(a,b){
+    resultado = a+b;
+    return resultado;
+} */
+
+function suma(a,b){
+    return alert(a+b);
+}
+
+// Reto de potencia 
+const powerOfTwo = (number) => {
+
+};
+
+module.exports = powerOfTwo;
+
