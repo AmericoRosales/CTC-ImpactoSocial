5c047027fec5b76068ea5e831fea1607becfe7c4
nacht <arosales@crackthecode.la>
2024-06-10 20:39:01 -0500

    version07

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-console.log(10);
-
-/*function suma(a,b){
-    resultado = a+b;
-    return resultado;
-} */
+/*console.log(10);
 
 function suma(a,b){
+    resultado = a+b;
+    return resultado;
+} 
+
+function suma(a,ba){
     return alert(a+b);
-}
+}a
 
 // Reto de potencia 
 const powerOfTwo = (number) => {
@@ -15,4 +15,10 @@ const powerOfTwo = (number) => {
 };
 
 module.exports = powerOfTwo;
+*/
 
+function saludo(user){
+    return(`Hola, ${user}!`);
+}
+
+export {saludo};
